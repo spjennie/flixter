a3060b5c745d3d5d27c7790656861443072651d2
Jennie Kang <spjennie@hotmail.com>
2019-10-18 21:56:47 -0400

    Initial Commit

---
+++ flixter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixter
+//
+//  Created by Jennie Kang on 10/18/19.
+//  Copyright © 2019 spjennie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
