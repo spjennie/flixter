06bd7217f7032e95d52f88350e549f7646c4c73e
Jennie Kang <spjennie@hotmail.com>
2019-10-26 01:00:23 -0400

    added second screen & hereo's movies

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Jennie Kang on 10/26/19.
+//  Copyright © 2019 spjennie. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
